79d36c6a922f8c652804a1ecdb0aaf39ff3b31d6
Fernando Perez-Garcia <fernando.perezgarcia.17@ucl.ac.uk>
2019-11-15 11:10:36 +0000

    Add support to receive an NRRD as input mask

--- models/deepinfer_prostate_needle_finder/segment.py
+++ models/deepinfer_prostate_needle_finder/segment.py
@@ -15,19 +15,36 @@ VOLUMES_DIR = Path('/volumes')
 OUTPUT_DIR = Path('/output')
 
 # Define path types
+INPUT_TYPE = click.Path(exists=True)
 INPUT_DIR_TYPE = click.Path(exists=True, file_okay=False)
 
 
 @click.command()
-@click.argument('dicom-dir', type=INPUT_DIR_TYPE)
-@click.argument('dicom-mask-dir', type=INPUT_DIR_TYPE)
+@click.argument(
+    'dicom-dir', type=INPUT_DIR_TYPE,
+)
+@click.argument(
+    'prostate-mask',
+    type=INPUT_TYPE,
+)
 @click.option('--debug/--no-debug', default=False, help='put output dirs in mounted volume')
-def main(dicom_dir, dicom_mask_dir, debug):
+def main(dicom_dir, prostate_mask, debug):
+    """
+    Biopsy needle trajectory and tip detection in intraoperative MRIs.
+
+    http://www.deepinfer.org/models/prostate-needle-finder/
+
+    dicom-dir is a path to a directory containing a DICOM series of the MRI
+
+    prostate-mask is a path to a directory containing a DICOM series
+    or to an NRRD file.
+    """
+
     import arterys
 
-    # Custom I/O setup
+    ## Custom I/O setup
     dicom_dir = Path(dicom_dir)
-    dicom_mask_dir = Path(dicom_mask_dir)
+    prostate_mask = Path(prostate_mask)
 
     volumes_dir = dicom_dir.parent if debug else VOLUMES_DIR
     volumes_dir.mkdir(exist_ok=True)
@@ -36,18 +53,25 @@ def main(dicom_dir, dicom_mask_dir, debug):
     input_volume_name = input_volume_stem + '.nrrd'
     input_volume_path = volumes_dir / input_volume_name
 
-    input_volume_mask_stem = 'input_volume_seg'
-    input_volume_mask_name = input_volume_mask_stem + '.nrrd'
-    input_volume_mask_path = volumes_dir / input_volume_mask_name
 
     output_volume_name = input_volume_stem + '{}_needle_seg.nrrd'
     output_volume_path = volumes_dir / output_volume_name
 
-    # DICOM to volumes
+    ## DICOM to volumes
+    # Prostate MRI
     arterys.dicomvert(dicom_dir, input_volume_path)
-    arterys.dicomvert(dicom_mask_dir, input_volume_mask_path)
 
-    # Run inference
+    # Prostate mask
+    if prostate_mask.is_dir():
+        dicom_mask_dir = prostate_mask
+        input_volume_mask_stem = 'input_volume_seg'
+        input_volume_mask_name = input_volume_mask_stem + '.nrrd'
+        input_volume_mask_path = volumes_dir / input_volume_mask_name
+        arterys.dicomvert(dicom_mask_dir, input_volume_mask_path)
+    elif prostate_mask.suffix == '.nrrd':
+        input_volume_mask_path = prostate_mask
+
+    ## Run inference
     cmdline = (
         'python3', '/deepinfer/fit.py',
         '--ModelName', 'prostate-needle-finder',
@@ -59,6 +83,7 @@ def main(dicom_dir, dicom_mask_dir, debug):
         '--verbose',
     )
     cmdline = [str(arg) for arg in cmdline]
+    arterys.utils.print_command(cmdline)
     call(cmdline)
 
     # Volumes to Arterys format
